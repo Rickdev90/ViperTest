b1cce81a9fdd53730cc16bcce013666cd3cd5a2c
Ricardo Olea <testsun905@gmail.com>
2021-07-22 21:25:09 -0500

    creating instances in presenter

--- Airplane/HomePresenter.swift
+++ Airplane/HomePresenter.swift
@@ -14,8 +14,19 @@ protocol HomePresentation{
 }
 
 class HomePresenter{
+    ///Como home presennter se va a cominicar con el view principal creamos un referencia debil
+    ///Por eso es conveniente que el protocolo herede de la clase class el protocolo
+    ///Creamos la instancias de los protocolos
+    weak var view: HomeView?
+    var interactor: HomeUseCase
+    var router: HomeRouting
     
-    
+    ///inicializamos
+    init(view: HomeView, interactor: HomeUseCase, router:HomeRouting){
+        self.view = view
+        self.interactor = interactor
+        self.router = router
+    }
     
 }
 
